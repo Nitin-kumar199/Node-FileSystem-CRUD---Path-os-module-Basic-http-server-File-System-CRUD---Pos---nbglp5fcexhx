206907bb35f2dbba577776de0bcefa08275cd2dd
Nitin Kumar <199nitinkumar@gmail.com>
2022-12-25 14:30:48 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
+	fs.writeFile('file.txt', 'World', (err)=>{
+		if(err){
+			console.log(err);
+			return;
+		}
+		console.log("File Created !");
+	});
 	// dont chnage function name
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
+	fs.readFile('file.txt', (err, data)=>{
+		if(err){
+			console.log(err);
+			return;
+		}
+		console.log(data);
+	});
 	// dont chnage function name
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
+	fs.writeFile('file.txt', 'Hello', (err)=>{
+		if(err){
+			console.log(err);
+			return;
+		}
+		console.log("File Updated !");
+	});
 	// dont chnage function name
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
+	fs.unlink('file.txt', (err)=>{
+		if(err){
+		console.log(err);
+		return;
+		}
+		console.log("File Deleted !");
+	});
 	// dont chnage function name
 }
 
 
 
-module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
